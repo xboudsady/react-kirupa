62a022f7b520fe500d519d89c3ab61c565182655
xboudsady <boudsadyx@gmail.com>
2019-04-10 20:22:56 +0200

    Added MenuContainer component

---
+++ slidingmenu/src/MenuContainer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class MenuContainer extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <p>Can you spot the item that doesn't belong?</p>
+          <ul>
+            <li>Lorem</li>
+            <li>Ipsum</li>
+            <li>Dolor</li>
+            <li>Sit</li>
+            <li>Bumblebees</li>
+            <li>Aenean</li>
+            <li>Consectetur</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MenuContainer;
