93f78d7cad5825991df219df635765ce9a07b8f0
xboudsady <boudsadyx@gmail.com>
2019-04-16 10:49:47 +0200

    Added 'exact path' to our Route for /Home

--- react_spa/src/Main.js
+++ react_spa/src/Main.js
@@ -8,19 +8,21 @@ import Contact from './Contact';
 class Main extends Component {
   render() {
     return(
-      <div>
-        <h1>Simple SPA</h1>
-        <ul className="header">
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/stuff">Stuff</NavLink></li>
-          <li><NavLink to="/contact">Contact</NavLink></li>
-        </ul>
-        <div className="content">
-          <Route path="/" component={Home} />
-          <Route path="/stuff" component={Stuff} />
-          <Route path="/contact" component={Contact} />
+      <HashRouter>
+        <div>
+          <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/stuff">Stuff</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home} />
+            <Route path="/stuff" component={Stuff} />
+            <Route path="/contact" component={Contact} />
+          </div>
         </div>
-    </div>
+      </HashRouter>
     );
   }
 }
