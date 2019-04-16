5257101380296ff3744752f81cb150d36bfcc084
xboudsady <boudsadyx@gmail.com>
2019-04-16 10:37:43 +0200

    Remove <a> element, and andded <NavLink> components

---
+++ react_spa/src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laudantium quod minus, mollitia excepturi soluta tempora magni distinctio qui reiciendis dolore explicabo ratione repudiandae. Aspernatur earum dolorem in nesciunt explicabo inventore quidem veritatis saepe, maiores natus nulla nemo sequi facere tempora voluptatum suscipit id?</p>
+        
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, iste.</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ react_spa/src/Main.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Route, NavLink, HashRouter } from 'react-router-dom';
+import Home from './Home';
+import Stuff from './Stuff';
+import Contact from './Contact';
+
+
+class Main extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Simple SPA</h1>
+        <ul className="header">
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/stuff">Stuff</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+      <div className="content">
+
+      </div>
+    </div>
+    );
+  }
+}
+
+export default Main;
