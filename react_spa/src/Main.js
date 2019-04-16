6b17f3d78e55fb0c0fd90a7a74c68a6c76062e61
xboudsady <boudsadyx@gmail.com>
2019-04-16 10:40:52 +0200

    Added Route Component

--- react_spa/src/Main.js
+++ react_spa/src/Main.js
@@ -15,9 +15,11 @@ class Main extends Component {
           <li><NavLink to="/stuff">Stuff</NavLink></li>
           <li><NavLink to="/contact">Contact</NavLink></li>
         </ul>
-      <div className="content">
-
-      </div>
+        <div className="content">
+          <Route path="/" component={Home} />
+          <Route path="/stuff" component={Stuff} />
+          <Route path="/contact" component={Contact} />
+        </div>
     </div>
     );
   }
